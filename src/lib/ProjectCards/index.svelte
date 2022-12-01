5fc89d62f389f555601e3905f0bb8559394c9ea6
Bruno Rosendo <brunomrosendo@hotmail.com>
2022-12-01 16:45:14 +0000

    Project cards

---
+++ src/lib/ProjectCards/index.svelte
@@ -0,0 +1,93 @@
+<script>
+// @ts-nocheck
+
+	import LayoutGrid, { Cell } from '@smui/layout-grid';
+	import List, { Item } from '@smui/list';
+    import Card, { Content } from '@smui/card';
+
+    export let project1, project2, project3;
+</script>
+
+<LayoutGrid fixedColumnWidth>
+    <Cell span={8}>
+        <div class="small-projects">
+            <div class="card-display card-dark">
+                <div class="small-card-container">
+                    <img src={project1.imageURL} alt={project1.name + " logo"} class="project-image project-image-small" />
+                    <div class="small-card-text">
+                        <div class="mdc-typography--headline4">{project1.description}</div>
+                        <div class="mdc-typography--headline5">{project1.type}</div>
+                    </div>
+                </div>
+            </div>
+            <div class="card-display card-light">
+                <div class="small-card-container">
+                    <img src={project3.imageURL} alt={project3.name + " logo"} class="project-image project-image-small" />
+                    <div class="small-card-text">
+                        <div class="mdc-typography--headline4">{project3.description}</div>
+                        <div class="mdc-typography--headline5">{project3.type}</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </Cell>
+    <Cell span={4}>
+        <div class="card-display card-middle">
+            <div class="card-wrapper">
+                <div>
+                    <img src={project2.imageURL} alt={project2.name + " logo"} class="project-image project-image-big" />
+                </div>
+                <div class="mdc-typography--headline3" style="margin-bottom: 4.5rem">{project2.description}</div>
+                <div class="mdc-typography--headline5">{project2.type}</div>
+            </div>
+        </div>
+    </Cell>
+</LayoutGrid>
+
+<style>
+    .card-display {
+        border: solid 1px;
+        filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
+        width: 100%;
+    }
+    .card-dark {
+        background: linear-gradient(#1F0000, #9B1515);
+        border-color: #BF0808;
+    }
+    .card-middle {
+        background: linear-gradient(#DA7F62, #75171E);
+        border-color: #FB3232;
+    }
+    .card-light {
+        background: linear-gradient(#7E2C12, #FF4A31);
+        border-color: #FF5B27;
+    }
+    .card-wrapper {
+        padding: 3rem;
+    }
+    .project-image {
+        filter: drop-shadow(0px 5px 11px rgba(60, 0, 0, 0.58));
+    }
+    .small-card-container {
+        display: flex;
+        justify-content: space-evenly;
+        padding: 3rem 0 3rem 0;
+    }
+    .project-image-big {
+        margin-bottom: 5rem;
+    }
+    .project-image-small {
+        height: 12rem;
+    }
+    .small-card-text {
+        display: flex;
+        flex-direction: column;
+        justify-content: space-evenly;
+    }
+    .small-projects {
+        height: 100%;
+        display: flex;
+        flex-direction: column;
+        justify-content:space-between
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,24 +1,31 @@
 <script>
-	import LayoutGrid, { Cell } from '@smui/layout-grid';
+	import ProjectCards from "$lib/ProjectCards/index.svelte";
+    import LayoutGrid, { Cell } from '@smui/layout-grid';
+
+    const project1 = {
+        name: "Project 1",
+        description: "A FEUP no teu bolso",
+        type: "Aplicação Mobile",
+        imageURL: "https://play-lh.googleusercontent.com/947VgiC4ySPiJtqMxhTDP7XuZs4UTWDtVYBsergfzM4XTbLA7mS4Xrh2lksZ28bm4nrQ=w240-h480",
+    };
+    const project2 = {
+        name: "Project 2",
+        description: "A FEUP no teu bolso",
+        type: "Aplicação Mobile",
+        imageURL: "https://play-lh.googleusercontent.com/947VgiC4ySPiJtqMxhTDP7XuZs4UTWDtVYBsergfzM4XTbLA7mS4Xrh2lksZ28bm4nrQ=w240-h480",
+    };
+    const project3 = {
+        name: "Project 3",
+        description: "A FEUP no teu bolso",
+        type: "Aplicação Mobile",
+        imageURL: "https://play-lh.googleusercontent.com/947VgiC4ySPiJtqMxhTDP7XuZs4UTWDtVYBsergfzM4XTbLA7mS4Xrh2lksZ28bm4nrQ=w240-h480",
+    };
 </script>
 
 <div class = "main-page">
     <div class="mdc-typography--headline3" style="margin: 2vw; font-size: 2.5rem">{"< Projetos />"}</div>
 
-    <LayoutGrid fixedColumnWidth style="border: 1px solid;">
-        <Cell span={8} align = "top" style = "border: 1px solid;">
-            <div class="mdc-typography--headline5">{"< Projeto 1 />"}</div>
-            <div class="mdc-typography--body1">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
-        </Cell>
-        <Cell span={4} style="height: 165%; border: 1px solid;">
-            <div class="mdc-typography--headline5">{"< Projeto 2 />"}</div>
-            <div class="mdc-typography--body1">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
-        </Cell>
-        <Cell span={8} align = "middle" style = "border: 1px solid;">
-            <div class="mdc-typography--headline5">{"< Projeto 3 />"}</div>
-            <div class="mdc-typography--body1">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
-        </Cell>
-    </LayoutGrid>   
+    <ProjectCards {project1} {project2} {project3} />
 
     <div class="mdc-typography--headline3" style="margin: 2vw; margin-top: 4vw;font-size: 2.5rem">{"< Projetos Arquivados />"}</div>
 
