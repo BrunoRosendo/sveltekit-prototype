6d991618ee56e8e11bcf26c1a90e64421ea21759
coutinho21 <guilhermecscoutinho@gmail.com>
2022-12-01 17:51:15 +0000

    Updated archived projects

---
+++ src/lib/ProjectCards/ArchivedProjectCard.svelte
@@ -0,0 +1,35 @@
+<script>
+// @ts-nocheck
+    export let project;
+</script>
+
+<div class="card-display">
+    <div class="small-card-text">
+        <div class="mdc-typography--headline4">{project.name}</div>
+        <div class="mdc-typography--headline5">{project.type}</div>
+    </div>
+</div>
+
+
+<style>
+    .card-display {
+        background-color: rgba(255, 255, 255, 0.25);
+        aspect-ratio: 1;
+        width: 80%;
+        border-radius: 2.5rem;
+        margin: 0 10% 0 10%;
+    }
+
+    .card-display:hover{
+        background: linear-gradient(rgba(179, 54, 54, 0.25),rgba(113, 3, 3, 0.25));
+        transition: .2s;
+    }
+    
+    .small-card-text{
+        height: 50%;
+        padding: 25% 0 25% 0;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-evenly;
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script>
-	import ProjectCards from "$lib/ProjectCards/index.svelte";
+	import ArchivedProjectCard from "$lib/ProjectCards/ArchivedProjectCard.svelte";
+import ProjectCards from "$lib/ProjectCards/index.svelte";
     import LayoutGrid, { Cell } from '@smui/layout-grid';
 
     const project1 = {
@@ -29,18 +30,15 @@
 
     <div class="mdc-typography--headline3" style="margin: 2vw; margin-top: 4vw;font-size: 2.5rem">{"< Projetos Arquivados />"}</div>
 
-    <LayoutGrid fixedColumnWidth style="border: 1px solid">
-        <Cell span={4} align = "top" style = "border: 1px solid;">
-            <div class="mdc-typography--headline5">{"< Projeto 1 />"}</div>
-            <div class="mdc-typography--body1">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
+    <LayoutGrid fixedColumnWidth>
+        <Cell span={4}>
+            <ArchivedProjectCard project = {project1} />
         </Cell>
-        <Cell span={4} style= "border: 1px solid;">
-            <div class="mdc-typography--headline5">{"< Projeto 2 />"}</div>
-            <div class="mdc-typography--body1">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
+        <Cell span={4}>
+            <ArchivedProjectCard project = {project2} />        
         </Cell>
-        <Cell span={4} align = "middle" style = "border: 1px solid;">
-            <div class="mdc-typography--headline5">{"< Projeto 3 />"}</div>
-            <div class="mdc-typography--body1">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
+        <Cell span={4}>
+            <ArchivedProjectCard project = {project3} />        
         </Cell>
     </LayoutGrid>   
 </div>
